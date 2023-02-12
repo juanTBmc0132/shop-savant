ed56e0487ad777745aadf7ee8071a386a5e213ee
Emilia Zielinska <eziel034@uottawa.ca>
2023-02-11 22:12:02 -0500

    Add Cohere API (#1)
    * Add API for cohere
    
    * Lower temperature and move secrets

---
+++ shop-savant-backend/nlp.py
@@ -0,0 +1,21 @@
+import cohere
+import sys
+
+f = open("./secrets.txt", "r")
+secret = f.read()
+f.close()
+
+co = cohere.Client(secret)
+
+def main(prompt):
+    response = co.generate(
+        prompt = prompt,
+        model = "command-medium-nightly",
+        max_tokens = 1000,
+        temperature = 0.25
+    )
+
+    print(response.generations[0].text)
+
+if __name__ == "__main__":
+    main(str(sys.argv[1]))
